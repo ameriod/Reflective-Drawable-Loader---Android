c4a5beb0ed4b9640579a4a9e73a1f7855f79e22e
parker <ameriod@gmail.com>
2015-12-05 16:34:26 -0500

    formatting code

--- lib/src/main/java/co/uk/alt236/reflectivedrawableloader/containers/DrawableResourceContainer.java
+++ lib/src/main/java/co/uk/alt236/reflectivedrawableloader/containers/DrawableResourceContainer.java
@@ -1,12 +1,12 @@
 /*******************************************************************************
  * Copyright 2013 Alexandros Schillings
- * 
+ * <p/>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
- *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ * <p/>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p/>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -21,68 +21,68 @@ import android.graphics.PorterDuff.Mode;
 import android.widget.ImageView;
 
 public class DrawableResourceContainer {
-	private final int mResourceId;
-	private final Integer mColourFilterColour;
-	private final String mDrawableName;
-	private ColorFilter mColourFilter;
+    private final int mResourceId;
+    private final Integer mColourFilterColour;
+    private final String mDrawableName;
+    private ColorFilter mColourFilter;
 
-	public DrawableResourceContainer(int resourceId, Integer colourFilterColour) {
-		this(null, resourceId, colourFilterColour);
-	}
+    public DrawableResourceContainer(int resourceId, Integer colourFilterColour) {
+        this(null, resourceId, colourFilterColour);
+    }
 
-	public DrawableResourceContainer(String drawableName, int resourceId, Integer colourFilterColour) {
-		this.mResourceId = resourceId;
-		this.mColourFilterColour = colourFilterColour;
-		this.mDrawableName = drawableName;
-	}
+    public DrawableResourceContainer(String drawableName, int resourceId, Integer colourFilterColour) {
+        this.mResourceId = resourceId;
+        this.mColourFilterColour = colourFilterColour;
+        this.mDrawableName = drawableName;
+    }
 
-	public Integer getColourFilterColour() {
-		return mColourFilterColour;
-	}
+    public Integer getColourFilterColour() {
+        return mColourFilterColour;
+    }
 
-	public String getDrawableName() {
-		return mDrawableName;
-	}
+    public String getDrawableName() {
+        return mDrawableName;
+    }
 
-	private ColorFilter getOverridingColorFilter(int color){
-		if(mColourFilter == null){
-			int red = (color & 0xFF0000) / 0xFFFF;
-			int green = (color & 0xFF00) / 0xFF;
-			int blue = color & 0xFF;
+    private ColorFilter getOverridingColorFilter(int color) {
+        if (mColourFilter == null) {
+            int red = (color & 0xFF0000) / 0xFFFF;
+            int green = (color & 0xFF00) / 0xFF;
+            int blue = color & 0xFF;
 
-			float[] matrix = { 0, 0, 0, 0, red
-					, 0, 0, 0, 0, green
-					, 0, 0, 0, 0, blue
-					, 0, 0, 0, 1, 0 };
+            float[] matrix = {0, 0, 0, 0, red
+                    , 0, 0, 0, 0, green
+                    , 0, 0, 0, 0, blue
+                    , 0, 0, 0, 1, 0};
 
-			mColourFilter =  new ColorMatrixColorFilter(matrix);
-		}
-		return mColourFilter;
-	}
+            mColourFilter = new ColorMatrixColorFilter(matrix);
+        }
+        return mColourFilter;
+    }
 
-	public int getResourceId() {
-		return mResourceId;
-	}
+    public int getResourceId() {
+        return mResourceId;
+    }
 
-	public boolean hasColourFilter() {
-		return !(mColourFilterColour == null);
-	}
+    public boolean hasColourFilter() {
+        return !(mColourFilterColour == null);
+    }
 
-	public void setDrawableWithColorOverrideMatrix(ImageView iv) {
-		iv.setImageResource(mResourceId);
-		if (hasColourFilter()) {
-			iv.setColorFilter(getOverridingColorFilter(mColourFilterColour));
-		} else {
-			iv.setColorFilter(null);
-		}
-	}
+    public void setDrawableWithColorOverrideMatrix(ImageView iv) {
+        iv.setImageResource(mResourceId);
+        if (hasColourFilter()) {
+            iv.setColorFilter(getOverridingColorFilter(mColourFilterColour));
+        } else {
+            iv.setColorFilter(null);
+        }
+    }
 
-	public void setDrawableWithPorterDuffMultiply(ImageView iv) {
-		iv.setImageResource(mResourceId);
-		if (hasColourFilter()) {
-			iv.setColorFilter(mColourFilterColour, Mode.MULTIPLY);
-		} else {
-			iv.setColorFilter(null);
-		}
-	}
+    public void setDrawableWithPorterDuffMultiply(ImageView iv) {
+        iv.setImageResource(mResourceId);
+        if (hasColourFilter()) {
+            iv.setColorFilter(mColourFilterColour, Mode.MULTIPLY);
+        } else {
+            iv.setColorFilter(null);
+        }
+    }
 }
--- lib/src/main/java/co/uk/alt236/reflectivedrawableloader/containers/LruLinkedHashMap.java
+++ lib/src/main/java/co/uk/alt236/reflectivedrawableloader/containers/LruLinkedHashMap.java
@@ -1,12 +1,12 @@
 /*******************************************************************************
  * Copyright 2013 Alexandros Schillings
- * 
+ * <p/>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
- *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ * <p/>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p/>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -16,24 +16,23 @@
 package co.uk.alt236.reflectivedrawableloader.containers;
 
 import java.util.LinkedHashMap;
-import java.util.Map;
 
-public class LruLinkedHashMap<K,V> extends LinkedHashMap<K,V>{
+public class LruLinkedHashMap<K, V> extends LinkedHashMap<K, V> {
 
-	private final int mMaxEntries;
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = -1575270872785732340L;
+    private final int mMaxEntries;
+    /**
+     *
+     */
+    private static final long serialVersionUID = -1575270872785732340L;
 
-	public LruLinkedHashMap(int maxEntries, float loadFactor){
-		super(maxEntries + 1, loadFactor, true);
-		this.mMaxEntries = maxEntries + 1;
-	}
+    public LruLinkedHashMap(int maxEntries, float loadFactor) {
+        super(maxEntries + 1, loadFactor, true);
+        this.mMaxEntries = maxEntries + 1;
+    }
 
-	@SuppressWarnings("rawtypes")
-	@Override
-	public boolean removeEldestEntry(Entry eldest) {
-		return size() > mMaxEntries;
-	}
+    @SuppressWarnings("rawtypes")
+    @Override
+    public boolean removeEldestEntry(Entry eldest) {
+        return size() > mMaxEntries;
+    }
 }
